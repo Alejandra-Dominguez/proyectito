27aa31d2da24b9ddc2e699cac97b11bb4f8e8bcd
Alejandra-Dominguez <alejandradomin02@gmail.com>
2025-04-05 12:19:11 -0600

    Estructura HTML para el index e inicio de agregar.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
     require 'db.php';
+    /*busca en la base de datos*/
+    $busqueda = $_GET['busqueda'] ?? '';
+    $param = "%busqueda%";
+    
+    $stmt = $pdo->prepare("SELECT * FROM gastos WHERE nombre LIKE ? or tipo LIKE ? ORDER BY fecha DESC");
+    $stmt ->execute([$param, $param]);
+    $gastos = $stmt->fetchAll();
+
+    $total = array_sum(array_colum($gastos, 'valor'));
+
 ?>
 
